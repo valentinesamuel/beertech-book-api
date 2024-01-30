c240e1f552d22cb6de260f18a330d7ba700bf42d
valentinesamuel <valentinesamuel2580@gmail.com>
2024-01-30 14:40:13 +0100

    (feat): added sqlite3 and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { Book } from './books/entities/book.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sql',
+      // synchronize: true,
+      entities: [Book],
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
